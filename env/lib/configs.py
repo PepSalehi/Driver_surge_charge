8c37d863dc718dfb93ec37e4041563462533869c
Peyman <p.noursalehi@gmail.com>
2020-06-01 17:15:19 -0400

    added gitignore

--- env/lib/configs.py
+++ env/lib/configs.py
@@ -1,4 +1,4 @@
-config_dict = dict(output_path="./Outputs/", BETA=2, FLEET_SIZE=4000, SURGE_MULTIPLIER=2, PERC_KNOW=0, BONUS=5,
+config_dict = dict(output_path="./Outputs/", BETA=2, FLEET_SIZE=3000, SURGE_MULTIPLIER=2, PERC_KNOW=0, BONUS=5,
                    MAX_BONUS=5, PRO_SHARE=0, BUDGET=10000,
-                   ANALYSIS_DURATION = 4*3600,
+                   ANALYSIS_DURATION=4 * 3600,
                    MAX_WAIT=5 * 60, BONUS_POLICY="SAC")
